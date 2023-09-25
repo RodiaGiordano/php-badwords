561224c6c9a019fa44a46cd74877db20aa1762a9
RodiaGiordano <rodiagiordano93@gmail.com>
2023-09-25 15:33:34 +0200

    fet: add structure

---
+++ result.php
@@ -0,0 +1,21 @@
+<?php
+
+$name = $_GET["name"];
+$password = $_GET["password"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo $name ?>
+    
+    <?php echo $password ?>
+    
+</body>
+</html>
